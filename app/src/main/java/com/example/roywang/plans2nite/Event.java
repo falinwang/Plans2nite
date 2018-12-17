514c8071cb074efc1ecc52e7caba8d759a6c7e03
dongtingw <dtwang@umich.edu>
2018-12-16 20:26:12 -0500

    created RecyclerViewAdapter.java - Amit and dongting
    Signed-off-by: dongtingw <dtwang@umich.edu>

--- app/src/main/java/com/example/roywang/plans2nite/Event.java
+++ app/src/main/java/com/example/roywang/plans2nite/Event.java
@@ -1,11 +1,11 @@
 package com.example.roywang.plans2nite;
 
 public class Event {
-    String name;
-    String date;
-    String location;
-    String plantype;
-    String details;
+    public String name;
+    public String date;
+    public String location;
+    public String plantype;
+    public String details;
 
     public Event(String name, String date, String location, String plantype, String details) {
         this.name = name;
---
+++ app/src/main/java/com/example/roywang/plans2nite/RecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.example.roywang.plans2nite;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Event> events;
+    private Context mContext;
+
+
+    RecyclerViewAdapter(ArrayList<Event> events, Context mContext) {
+        this.events = events;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
+        viewHolder.textViewName.setText(events.get(i).name);
+        viewHolder.textViewDate.setText(events.get(i).date);
+        viewHolder.textViewLocation.setText(events.get(i).location);
+        viewHolder.textViewPlantype.setText(events.get(i).plantype);
+        viewHolder.textViewDetails.setText(events.get(i).details);
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, events.get(i).name, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, events.get(i).date, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, events.get(i).location, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, events.get(i).plantype, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, events.get(i).details, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewName;
+        TextView textViewDate;
+        TextView textViewLocation;
+        TextView textViewPlantype;
+        TextView textViewDetails;
+
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewDate = itemView.findViewById(R.id.textViewDate);
+            textViewLocation = itemView.findViewById(R.id.textViewLocation);
+            textViewPlantype = itemView.findViewById(R.id.textViewPlantype);
+            textViewDetails = itemView.findViewById(R.id.textViewDetails);
+            parentLayout = itemView.findViewById(R.id.parent_Layout);
+
+        }
+    }
+}
