b44d1febd8f18c44ed61dd7854241eb75787d363
amitmishra <amitmsra@umich.edu>
2018-12-17 20:47:26 -0500

    Updated the codes for user activity

--- app/src/main/java/com/example/roywang/plans2nite/EditProfileActivity.java
+++ app/src/main/java/com/example/roywang/plans2nite/EditProfileActivity.java
@@ -3,10 +3,14 @@ package com.example.roywang.plans2nite;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,8 +18,9 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProfileActivity extends Activity implements View.OnClickListener {
 
-    EditText editTextName1,editTextLocation1,editTextEmail1,editTextContact1;
+    EditText editTextName1,editTextLocation1,editTextGender1 ,editTextContact1;
     Button buttonSave,buttonViewProfile;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -25,7 +30,7 @@ public class EditProfileActivity extends Activity implements View.OnClickListene
         setContentView(R.layout.activity_edit_profile);
 
         editTextContact1 =findViewById(R.id.editTextContact1) ;
-        editTextEmail1= findViewById(R.id.editTextEmail1  );
+        editTextGender1= findViewById(R.id.editTextGender1   );
         editTextLocation1=findViewById(R.id.editTextLocation1 ) ;
         editTextName1=findViewById(R.id.editTextName1 );
 
@@ -35,10 +40,48 @@ public class EditProfileActivity extends Activity implements View.OnClickListene
         buttonSave.setOnClickListener(this);
         buttonViewProfile.setOnClickListener(this);
 
-
+        mAuth = FirebaseAuth.getInstance();
 
 
         }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater optionsMenuInflater = getMenuInflater();
+        optionsMenuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ListView_Menu:
+                Intent ListViewMenuInt = new Intent(EditProfileActivity .this,ListViewActivity.class);
+                startActivity(ListViewMenuInt);
+                return true;
+
+            case R.id.MyPlans_Menu:
+                Intent MyPlansMenuInt = new Intent(EditProfileActivity .this,UpcomingPlanListActivity.class);
+                startActivity(MyPlansMenuInt);
+                return true;
+
+            case R.id.InputPlans_Menu:
+                Intent InputPlanInt = new Intent(EditProfileActivity .this,PlanInputActivity .class);
+                startActivity(InputPlanInt);
+                return true;
+
+            case R.id.LogOut_Menu:
+                Intent LogOutMenuInt = new Intent(EditProfileActivity .this,LoginActivity.class);
+                startActivity(LogOutMenuInt);
+                return true;
+
+            case R.id.Profile_Menu :
+                return true;
+
+            default:
+                return false;
+        }
+    }
 
     @Override
     public void onClick(View v) {
@@ -50,11 +93,12 @@ public class EditProfileActivity extends Activity implements View.OnClickListene
 
         {
             String createName=editTextName1.getText().toString();
-            String createEmail=editTextEmail1.getText().toString();
+            String createEmail=mAuth.getCurrentUser().getEmail() ;
             String createLocation=editTextLocation1.getText().toString();
             String createContact=editTextContact1.getText().toString();
+            String createGender=editTextGender1.getText().toString();
 
-            User newUser=new User(createName,createLocation,createEmail ,createContact );
+            User newUser=new User(createName,createLocation,createEmail ,createContact,createGender  );
 
             myRef.push().setValue(newUser);
 
--- app/src/main/java/com/example/roywang/plans2nite/User.java
+++ app/src/main/java/com/example/roywang/plans2nite/User.java
@@ -6,13 +6,16 @@ public class User {
     public String location;
     public String email;
     public String contact;
+    public String gender;
 
-    public User(String name,String location, String email, String contact)
+    public User(String name,String location, String email, String contact,String gender)
     {
         this.name=name;
         this.location=location;
         this.email=email;
         this.contact=contact;
+        this.gender=gender;
+
 
     }
 
